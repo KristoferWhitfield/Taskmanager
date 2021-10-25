6b8fa0abe0f0d54aa7895144cd91a22695c19f04
Kristofer Whitfield <78818078+KristoferWhitfield@users.noreply.github.com>
2021-10-25 10:58:42 -0400

    database

---
+++ app/secrets.js
@@ -0,0 +1,9 @@
+// module.exports = {
+//     url: `mongodb+srv://KrisW:Dogwistle2@cluster0.bgwen.mongodb.net/tasks-manager?retryWrites=true&w=majority`
+// }
+
+module.exports = {
+  url:
+    "mongodb+srv://KW1234:test1234@cluster0.jcysh.mongodb.net/tasks-manager?retryWrites=true&w=majority",
+  dbName: "demo",
+};
